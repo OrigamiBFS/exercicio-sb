c8435f4acf58c8ed0255ef18ba5ede0eda1bc0f0
OrigamiBFS <87385136+OrigamiBFS@users.noreply.github.com>
2022-07-20 13:56:32 -0300

    Add files via upload

---
+++ exercicio-sb/src/test/java/com/example/exerciciosb/controllers/PrimeiroController.java
@@ -0,0 +1,14 @@
+package com.example.exerciciosb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	@GetMapping("/")
+	public String ola() {
+		return "Olá, Spring Boot";
+	}
+
+}
